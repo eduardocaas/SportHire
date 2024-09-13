3a3ee59779dfc46a16ba7ae4bf05a4dda0c83594
eduardocaas <eduardofc.dev@gmail.com>
2024-09-13 19:54:07 -0300

    Implemented Sha256 Hash function

--- SportHire.Identity/SportHire.Identity.Infrastructure/Security/AuthService.cs
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Security/AuthService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,21 @@ namespace SportHire.Identity.Infrastructure.Security
     {
         public string GenerateSha256Hash(string password)
         {
-            throw new NotImplementedException();
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // create sha256 hash 
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder builder = new StringBuilder();
+                // convert byte array to string
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                // return hashed password string
+                return builder.ToString();
+            }
         }
     }
 }
