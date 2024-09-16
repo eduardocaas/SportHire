915fb918519ac7c62a42effaa229c6b40f159efc
eduardocaas <eduardofc.dev@gmail.com>
2024-09-15 23:30:06 -0300

    Add ver1.sql

---
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Persistence/Scripts/ver1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE "Users" (
+    "Id" uuid NOT NULL DEFAULT (gen_random_uuid()),
+    "FullName" VARCHAR NOT NULL,
+    "Email" VARCHAR NOT NULL,
+    "Password" VARCHAR NOT NULL,
+    "EmailConfirmed" BOOLEAN NOT NULL DEFAULT (FALSE),
+    CONSTRAINT "PK_Users" PRIMARY KEY ("Id")
+);
