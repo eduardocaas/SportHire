0887c77b67df584be8ee65e4ec91854c69e6bdc6
eduardocaas <eduardofc.dev@gmail.com>
2024-09-15 23:27:56 -0300

    Add 'ver1' migration

---
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Migrations/20240916022050_ver1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportHire.Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ver1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    FullName = table.Column<string>(type: "VARCHAR", maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "VARCHAR", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "VARCHAR", maxLength: 255, nullable: false),
+                    EmailConfirmed = table.Column<bool>(type: "BOOLEAN", nullable: false, defaultValueSql: "FALSE")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
