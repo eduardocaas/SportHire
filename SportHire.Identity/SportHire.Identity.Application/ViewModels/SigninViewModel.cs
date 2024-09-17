d07cc9ec470dc3a5be156a2dbc677044b3e98366
eduardocaas <eduardofc.dev@gmail.com>
2024-09-16 23:42:17 -0300

    Add ctor and set properties

--- SportHire.Identity/SportHire.Identity.Application/ViewModels/SigninViewModel.cs
+++ SportHire.Identity/SportHire.Identity.Application/ViewModels/SigninViewModel.cs
@@ -8,5 +8,13 @@ namespace SportHire.Identity.Application.ViewModels
 {
     public class SigninViewModel
     {
+        public SigninViewModel(string email, string token)
+        {
+            Email = email;
+            Token = token;
+        }
+
+        public string Email { get; private set; }
+        public string Token { get; private set; }
     }
 }
