add0cd37014be91b467df8eda6607343b9f1d0ec
eduardocaas <eduardofc.dev@gmail.com>
2024-09-16 23:41:46 -0300

    Add SigninViewModel.cs

---
+++ SportHire.Identity/SportHire.Identity.Application/ViewModels/SigninViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportHire.Identity.Application.ViewModels
+{
+    public class SigninViewModel
+    {
+    }
+}
