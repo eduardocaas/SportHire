9244cb7cd0188641f9af5e2dc57d3cd359004bcf
eduardocaas <eduardofc.dev@gmail.com>
2024-09-17 11:06:35 -0300

    Add methods AddAsync and GetUser

--- SportHire.Identity/SportHire.Identity.Core/Repositories/IUserRepository.cs
+++ SportHire.Identity/SportHire.Identity.Core/Repositories/IUserRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace SportHire.Identity.Core.Repositories
+﻿using SportHire.Identity.Core.Entities;
+
+namespace SportHire.Identity.Core.Repositories
 {
     public interface IUserRepository
     {
+        Task AddAsync(User user);
+        Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash);
     }
 }
