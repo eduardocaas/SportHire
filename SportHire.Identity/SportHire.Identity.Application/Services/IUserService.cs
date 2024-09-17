d165b1acfe696e6a063139a714a3fc843b0cd706
eduardocaas <eduardofc.dev@gmail.com>
2024-09-16 23:48:22 -0300

    Add methods SignIn and SignUp

--- SportHire.Identity/SportHire.Identity.Application/Services/IUserService.cs
+++ SportHire.Identity/SportHire.Identity.Application/Services/IUserService.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SportHire.Identity.Application.InputModels;
+using SportHire.Identity.Application.ViewModels;
 
 namespace SportHire.Identity.Application.Services
 {
     public interface IUserService
     {
+        SigninViewModel SignIn(SigninInputModel inputModel);
+        int SignUp(SignupInputModel inputModel);
     }
 }
