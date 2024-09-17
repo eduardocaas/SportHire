7c382e196f5a8db558376c2d3b53bd5269497c57
eduardocaas <eduardofc.dev@gmail.com>
2024-09-16 23:48:56 -0300

    Implements interface IUserService

--- SportHire.Identity/SportHire.Identity.Application/Services/UserService.cs
+++ SportHire.Identity/SportHire.Identity.Application/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SportHire.Identity.Application.InputModels;
+using SportHire.Identity.Application.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace SportHire.Identity.Application.Services
 {
-    internal class UserService
+    internal class UserService : IUserService
     {
+        public SigninViewModel SignIn(SigninInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SignUp(SignupInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
