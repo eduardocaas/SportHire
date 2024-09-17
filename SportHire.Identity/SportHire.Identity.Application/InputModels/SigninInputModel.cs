4253aef3a7b071e2e62810ed62d8caad74bfd82c
eduardocaas <eduardofc.dev@gmail.com>
2024-09-16 23:39:59 -0300

    Set properties

--- SportHire.Identity/SportHire.Identity.Application/InputModels/SigninInputModel.cs
+++ SportHire.Identity/SportHire.Identity.Application/InputModels/SigninInputModel.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace SportHire.Identity.Application.InputModels
 {
-    internal class SigninInputModel
+    public class SigninInputModel
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
