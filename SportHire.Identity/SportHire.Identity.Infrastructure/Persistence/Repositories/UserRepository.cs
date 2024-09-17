98619a8c43feadc65fddee0a092806913099dcfc
eduardocaas <eduardofc.dev@gmail.com>
2024-09-17 11:08:33 -0300

    Implemented interface IUserRepository

--- SportHire.Identity/SportHire.Identity.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SportHire.Identity.Core.Entities;
+using SportHire.Identity.Core.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace SportHire.Identity.Infrastructure.Persistence.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        public Task AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
