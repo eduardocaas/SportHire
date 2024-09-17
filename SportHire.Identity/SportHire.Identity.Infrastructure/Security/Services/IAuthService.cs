872d84ab1e920dbdd79c1ebd1d662337ee5f0938
eduardocaas <eduardofc.dev@gmail.com>
2024-09-17 11:27:53 -0300

    Change path

--- SportHire.Identity/SportHire.Identity.Infrastructure/Security/Services/IAuthService.cs
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Security/Services/IAuthService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SportHire.Identity.Infrastructure.Authentication
+namespace SportHire.Identity.Infrastructure.Security.Services
 {
     public interface IAuthService
     {
