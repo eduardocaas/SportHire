895720f8710a4ebd3dc54888dc6833382295d412
eduardocaas <eduardofc.dev@gmail.com>
2024-09-18 11:41:19 -0300

    Add constructor

--- SportHire.Identity/SportHire.Identity.Core/Entities/User.cs
+++ SportHire.Identity/SportHire.Identity.Core/Entities/User.cs
@@ -8,6 +8,13 @@ namespace SportHire.Identity.Core.Entities
 {
     public class User
     {
+        public User(string fullName, string email, string password)
+        {
+            FullName = fullName;
+            Email = email;
+            Password = password;
+        }
+
         public Guid Id { get; private set; }
         public string FullName { get; private set; }
         public string Email { get; private set; }
