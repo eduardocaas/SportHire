2ad962744518e12d27d977e0c490929b6e2a5cd0
eduardocaas <eduardofc.dev@gmail.com>
2024-09-19 11:42:22 -0300

    Add ValidationFilter injection

--- SportHire.Identity/SportHire.Identity.API/Program.cs
+++ SportHire.Identity/SportHire.Identity.API/Program.cs
@@ -1,3 +1,8 @@
+using FluentValidation;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+using SportHire.Identity.API.Filters;
+using SportHire.Identity.Application.InputModels;
+using SportHire.Identity.Application.Validators;
 using SportHire.Identity.Core.Repositories;
 using SportHire.Identity.Infrastructure.Persistence;
 using SportHire.Identity.Infrastructure.Persistence.Repositories;
@@ -7,15 +12,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add(typeof(ValidationFilter)));
+builder.Services.AddFluentValidationAutoValidation();
+// builder.Services.AddValidatorsFromAssemblyContaining<SignupInputModelValidator>(); alternative
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<IdentityDbContext>();
 
+// Repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+// Services
 builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Validators
+builder.Services.AddScoped<IValidator<SignupInputModel>, SignupInputModelValidator>();
 
 var app = builder.Build();
 
