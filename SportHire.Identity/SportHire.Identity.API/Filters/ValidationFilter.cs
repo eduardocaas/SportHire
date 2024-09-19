ce93e5451e406c72f0a580e937f13b62cb714508
eduardocaas <eduardofc.dev@gmail.com>
2024-09-18 23:32:50 -0300

    Add IActionFilter interface

--- SportHire.Identity/SportHire.Identity.API/Filters/ValidationFilter.cs
+++ SportHire.Identity/SportHire.Identity.API/Filters/ValidationFilter.cs
@@ -1,6 +1,17 @@
-﻿namespace SportHire.Identity.API.Filters
+﻿using Microsoft.AspNetCore.Mvc.Filters
+
+namespace SportHire.Identity.API.Filters
 {
-    public class ValidationFilter
+    public class ValidationFilter : IActionFilter
     {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
