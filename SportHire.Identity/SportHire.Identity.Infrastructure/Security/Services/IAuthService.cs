b5eabacd316d05bf1913e9ad04e4b81ba3a0526a
eduardocaas <eduardofc.dev@gmail.com>
2024-09-19 23:13:09 -0300

    Add method GenerateJwtToken

--- SportHire.Identity/SportHire.Identity.Infrastructure/Security/Services/IAuthService.cs
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Security/Services/IAuthService.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SportHire.Identity.Infrastructure.Security.Services
+﻿namespace SportHire.Identity.Infrastructure.Security.Services
 {
     public interface IAuthService
     {
         string GenerateSha256Hash(string password);
+        string GenerateJwtToken(string email);
     }
 }
