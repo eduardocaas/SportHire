8dd77020fbab79b2308e0327e8c121a5d01bc9d9
eduardocaas <eduardofc.dev@gmail.com>
2024-09-21 23:06:20 -0300

    Implemented Login endpoint

--- SportHire.Identity/SportHire.Identity.API/Controllers/UsersController.cs
+++ SportHire.Identity/SportHire.Identity.API/Controllers/UsersController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SportHire.Identity.Application.InputModels;
 using SportHire.Identity.Application.Services;
 
 namespace SportHire.Identity.API.Controllers
 {
     [ApiController]
     [Route("identity/[controller]")]
+    [Authorize]
     public class UsersController : ControllerBase
     {
         private readonly IUserService _userService;
@@ -12,6 +15,18 @@ namespace SportHire.Identity.API.Controllers
         public UsersController(IUserService userService)
         {
             _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] SigninInputModel inputModel)
+        {
+            var loginViewModel = await _userService.SignIn(inputModel);
+
+            return loginViewModel is null ? BadRequest("Login ou senha incorretos!") : Ok(loginViewModel); 
+
+            
         }
     }
 }
