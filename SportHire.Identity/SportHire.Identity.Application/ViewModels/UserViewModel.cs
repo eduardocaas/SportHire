ff9bbd725aed6a180ca320f1f8316d8d4816711c
eduardocaas <eduardofc.dev@gmail.com>
2024-09-22 16:09:40 -0300

    Add properties and constructor

--- SportHire.Identity/SportHire.Identity.Application/ViewModels/UserViewModel.cs
+++ SportHire.Identity/SportHire.Identity.Application/ViewModels/UserViewModel.cs
@@ -2,5 +2,13 @@
 {
     public class UserViewModel
     {
+        public UserViewModel(string fullName, string email)
+        {
+            FullName = fullName;
+            Email = email;
+        }
+
+        public string FullName { get; private set; }
+        public string Email { get; private set; }
     }
 }
