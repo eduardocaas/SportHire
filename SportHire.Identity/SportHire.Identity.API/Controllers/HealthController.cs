b913784be67786589efa2f91e38e76b32f46fc6f
eduardocaas <eduardofc.dev@gmail.com>
2024-09-23 21:55:41 -0300

    Add Authorization

--- SportHire.Identity/SportHire.Identity.API/Controllers/HealthController.cs
+++ SportHire.Identity/SportHire.Identity.API/Controllers/HealthController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SportHire.Identity.API.Controllers
 {
     [ApiController]
     [Route("identity/[controller]")]
+    [Authorize]
     public class HealthController : ControllerBase
     {
         [HttpGet]
