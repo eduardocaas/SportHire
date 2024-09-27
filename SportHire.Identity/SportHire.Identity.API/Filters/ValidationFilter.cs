5df84d83078fce6452d803fc2ebb1d1b99be37f6
eduardocaas <eduardofc.dev@gmail.com>
2024-09-26 22:59:02 -0300

    Add FluentValidation errors support

--- SportHire.Identity/SportHire.Identity.API/Filters/ValidationFilter.cs
+++ SportHire.Identity/SportHire.Identity.API/Filters/ValidationFilter.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportHire.Identity.API.Filters
 {
@@ -17,6 +18,14 @@ namespace SportHire.Identity.API.Filters
                     .SelectMany(ms => ms.Value.Errors)
                     .Select(e => e.ErrorMessage)
                     .ToList();
+
+                var fluentValidationErrors = context.ActionArguments
+                    .Where(arg => arg.Value is IValidatableObject)
+                    .SelectMany(arg => ((IValidatableObject)arg.Value).Validate(new ValidationContext(arg.Value)))
+                    .Select(v => v.ErrorMessage)
+                    .ToList();
+
+                messages.AddRange(fluentValidationErrors);
 
                 context.Result = new BadRequestObjectResult(messages);
             }
