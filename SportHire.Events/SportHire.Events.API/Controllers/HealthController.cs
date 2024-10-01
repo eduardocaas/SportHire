c84c328a90e14031ace826ab30ffbd57d87f84dc
eduardocaas <eduardofc.dev@gmail.com>
2024-09-30 22:46:49 -0300

    Add endpoint Status

--- SportHire.Events/SportHire.Events.API/Controllers/HealthController.cs
+++ SportHire.Events/SportHire.Events.API/Controllers/HealthController.cs
@@ -1,6 +1,14 @@
-﻿namespace SportHire.Events.API.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportHire.Events.API.Controllers
 {
-    public class HealthController
+    [ApiController]
+    [Route("events/[controller]")]
+    [Authorize]
+    public class HealthController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Status() => Ok("API is up to date");
     }
 }
