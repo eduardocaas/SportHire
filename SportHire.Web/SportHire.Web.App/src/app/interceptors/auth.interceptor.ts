3bf507e0357118a1af91db195ecce19f07cfc026
Eduardo Faligurski Castro <efc06@trf4.jus.br>
2024-10-04 14:44:14 -0300

    Add AuthInterceptor

---
+++ SportHire.Web/SportHire.Web.App/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,16 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+      let token = localStorage.getItem('token');
+
+      if (token) {
+        const cloneReq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) });
+      }
+  }
+}
