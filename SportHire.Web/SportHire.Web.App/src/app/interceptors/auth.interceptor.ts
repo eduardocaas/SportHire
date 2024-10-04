2e5dce44d9dd8f15a5274392c95e3dcc97565052
Eduardo Faligurski Castro <efc06@trf4.jus.br>
2024-10-04 14:49:33 -0300

    Implemented handler

--- SportHire.Web/SportHire.Web.App/src/app/interceptors/auth.interceptor.ts
+++ SportHire.Web/SportHire.Web.App/src/app/interceptors/auth.interceptor.ts
@@ -3,14 +3,22 @@ import { Observable } from "rxjs";
 
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-      let token = localStorage.getItem('token');
+    let token = localStorage.getItem('token');
 
-      if (token) {
-        const cloneReq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) });
-      }
+    if (token) {
+      const cloneReq = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) });
+      return next.handle(cloneReq);
+    }
+    else {
+      return next.handle(req);
+    }
   }
 }
+
+export const AuthInterceptorProvider = [{
+
+}]
