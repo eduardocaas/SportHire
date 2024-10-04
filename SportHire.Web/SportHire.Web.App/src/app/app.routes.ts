c490e3aa68b6fc03934bb91073adc9f41fc28991
Eduardo Faligurski Castro <efc06@trf4.jus.br>
2024-10-04 16:25:21 -0300

    Removed NavComponent import

--- SportHire.Web/SportHire.Web.App/src/app/app.routes.ts
+++ SportHire.Web/SportHire.Web.App/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
-import { NavComponent } from './components/nav/nav.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupComponent },
