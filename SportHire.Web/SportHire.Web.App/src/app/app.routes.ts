81ef9094ab6c97bfd071e16491a2095fd941d741
eduardocaas <eduardofc.dev@gmail.com>
2024-10-04 11:44:55 -0300

    Add Signup and Login routes

--- SportHire.Web/SportHire.Web.App/src/app/app.routes.ts
+++ SportHire.Web/SportHire.Web.App/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
 
 export const routes: Routes = [
-
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent }
 ];
