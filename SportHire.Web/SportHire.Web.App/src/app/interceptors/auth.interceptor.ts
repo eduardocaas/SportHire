f65885e4f57de07988214bdb0759dd262cb67f50
eduardocaas <eduardofc.dev@gmail.com>
2024-10-04 23:02:06 -0300

    Implemented method intercept

--- SportHire.Web/SportHire.Web.App/src/app/interceptors/auth.interceptor.ts
+++ SportHire.Web/SportHire.Web.App/src/app/interceptors/auth.interceptor.ts
@@ -5,8 +5,18 @@ import { Observable } from "rxjs";
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    throw new Error("Method not implemented.");
-  }
+  constructor() { }
 
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    let token = localStorage.getItem('token');
+
+    if (token) {
+      const reqClone = req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) });
+      return next.handle(reqClone);
+    }
+    else {
+      return next.handle(req);
+    }
+  }
 }
