75d56a483c6bcc0d5cfd09058cd51e04f0a71806
eduardocaas <eduardofc.dev@gmail.com>
2024-10-05 19:56:52 -0300

    Add signup and login components

--- SportHire.Web/SportHire.Web.App/src/app/app.module.ts
+++ SportHire.Web/SportHire.Web.App/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
