06dc3392e44b8504f1a7a5cc97f0120ab98d38a0
eduardocaas <eduardofc.dev@gmail.com>
2024-10-05 22:02:10 -0300

    Implemented method isAuthenticated

--- SportHire.Web/SportHire.Web.App/src/app/services/auth.service.ts
+++ SportHire.Web/SportHire.Web.App/src/app/services/auth.service.ts
@@ -2,11 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Credentials } from '../models/credentials';
 import { IDENTITY_CONFIG } from '../configs/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
 
@@ -19,5 +22,13 @@ export class AuthService {
 
   successfulLogin(authToken: string) {
     localStorage.setItem('token', authToken);
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token');
+    if (token != null) {
+      return !this.jwtService.isTokenExpired(token);
+    }
+    return false;
   }
 }
