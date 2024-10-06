d210c3ab3c3d9245bb6a7d078d9033d6a30c4cd7
eduardocaas <eduardofc.dev@gmail.com>
2024-10-05 21:56:32 -0300

    Implemented method authenticate

--- SportHire.Web/SportHire.Web.App/src/app/services/auth.service.ts
+++ SportHire.Web/SportHire.Web.App/src/app/services/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Credentials } from '../models/credentials';
+import { IDENTITY_CONFIG } from '../configs/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  authenticate(creds: Credentials) {
+    return this.http.post(`${IDENTITY_CONFIG.localUrl}/users/login`, creds, {
+      observe: 'response',
+      responseType: 'text'
+    });
+  }
 }
