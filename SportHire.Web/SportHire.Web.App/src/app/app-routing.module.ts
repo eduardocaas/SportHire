0d032ffc3695b28e8fa0cb411fad41df45f82e0b
eduardocaas <eduardofc.dev@gmail.com>
2024-10-05 23:35:30 -0300

    Add root path with AuthGuard

--- SportHire.Web/SportHire.Web.App/src/app/app-routing.module.ts
+++ SportHire.Web/SportHire.Web.App/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
+import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {
+    path: '', component: NavComponent, canActivate: [authGuard], children: []
+  }
 ];
 
 @NgModule({
