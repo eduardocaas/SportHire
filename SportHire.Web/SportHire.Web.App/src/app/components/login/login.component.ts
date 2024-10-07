f711388c291981369dd3d38ae47e33c550a3f741
eduardocaas <eduardofc.dev@gmail.com>
2024-10-06 23:29:21 -0300

    Add formControl and Validators for email

--- SportHire.Web/SportHire.Web.App/src/app/components/login/login.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.emailControl.hasError('required')) {
+      return 'Insira um email';
+    }
+    if (this.emailControl.hasError('email')) {
+      return 'Insira um email válido';
+    }
+  }
 }
