b548a1b44580cdb70b67a310f8c465df7d0faa3d
Eduardo Faligurski Castro <efc06@trf4.jus.br>
2024-10-07 16:34:48 -0300

    Add fields and form control

--- SportHire.Web/SportHire.Web.App/src/app/components/signup/signup.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  nameControl = new FormControl('', [Validators.required]);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordControl = new FormControl('', [Validators.required]);
+
+  // TODO: Adicionar Regex para caracteres especiais e maxLength
+  getErrorMessageName() {
+    return this.nameControl.hasError('required') ? 'Insira um nome' : '';
+  }
+
+  getErrorMessage() {
+    if (this.emailControl.hasError('required')) {
+      return 'Insira um email';
+    }
+
+    return this.emailControl.hasError('email') ? 'Insira um email válido' : '';
+  }
+
+  getErrorMessagePwd() {
+    return this.passwordControl.hasError('required') ? 'Insira uma senha' : '';
+  }
+
+  hide = true;
 }
