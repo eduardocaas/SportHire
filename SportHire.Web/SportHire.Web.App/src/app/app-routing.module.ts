6bd58130f862bfdd1b4af7cf227761131e81cb26
Eduardo Faligurski Castro <efc06@trf4.jus.br>
2024-10-11 14:42:24 -0300

    Add route for events-find Component

--- SportHire.Web/SportHire.Web.App/src/app/app-routing.module.ts
+++ SportHire.Web/SportHire.Web.App/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { authGuard } from './auth/auth.guard';
+import { EventsFindComponent } from './components/events/events-find/events-find.component';
 
 const routes: Routes = [
 
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
   {
-    path: '', component: NavComponent, children: []
+    path: '', component: NavComponent, children: [
+      { path: 'find', component: EventsFindComponent }
+    ]
   }
 ];
 
