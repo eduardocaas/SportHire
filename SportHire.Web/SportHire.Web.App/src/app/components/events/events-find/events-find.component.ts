2c5edb636ef25b67bc9747668a793e264b99ef89
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-12 17:49:49 -0300

    Add State field and options

--- SportHire.Web/SportHire.Web.App/src/app/components/events/events-find/events-find.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/events/events-find/events-find.component.ts
@@ -10,24 +10,42 @@ import { map, Observable, startWith } from 'rxjs';
 export class EventsFindComponent implements OnInit {
 
   sportControl = new FormControl('', [Validators.required]);
+  stateControl = new FormControl('', [Validators.required]);
 
   sportOptions: string[] = ['Basquete', 'Futebol', 'Futsal', 'Vôlei', 'Vôlei de praia'];
   sportFilteredOptions: Observable<string[]> = new Observable<string[]>;
 
+  stateOptions: string[] = ['RS'];
+  stateFilteredOptions: Observable<string[]> = new Observable<string[]>;
+
   ngOnInit() {
     this.sportFilteredOptions = this.sportControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value || '')),
+      map(value => this._sportFilter(value || '')),
+    );
+    this.stateFilteredOptions = this.stateControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._stateFilter(value || ''))
     );
   }
 
-  private _filter(value: string): string[] {
+  private _sportFilter(value: string): string[] {
     const filterValue = value.toLowerCase();
 
     return this.sportOptions.filter(option => option.toLowerCase().includes(filterValue));
   }
 
+  private _stateFilter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.stateOptions.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
   getErrorMessageSport() {
     return this.sportControl.hasError('required') ? 'Selecione um esporte' : '';
+  }
+
+  getErrorMessageState() {
+    return this.stateControl.hasError('required') ? 'Selecione um estado' : '';
   }
 }
