6f2b7f154525c939e28f1bdf6d02e26e961fd3da
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-12 00:29:09 -0300

    Add default structure for Autocomplete form

--- SportHire.Web/SportHire.Web.App/src/app/components/events/events-find/events-find.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/events/events-find/events-find.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-events-find',
   templateUrl: './events-find.component.html',
   styleUrl: './events-find.component.css'
 })
-export class EventsFindComponent {
+export class EventsFindComponent implements OnInit {
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> = new Observable<string[]>;
 
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
 }
