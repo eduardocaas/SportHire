2ed0be09d664e305ed6677cb0d07ba7e6d6b12a2
eduardocaas <eduardofc.dev@gmail.com>
2024-10-21 11:47:47 -0300

    Implemented fields

--- SportHire.Events/SportHire.Events.Core/Entities/Event.cs
+++ SportHire.Events/SportHire.Events.Core/Entities/Event.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using SportHire.Events.Core.Enums;
 
 namespace SportHire.Events.Core.Entities
 {
     public class Event
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string EmailOwner { get; set; }
+        public string? EmailPlayer { get; set; } = null;
+        public EventSportEnum Sport { get; set; }
+        public UF Uf { get; set; }
+        public string City { get; set; }
+        public string Neighborhood { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Duration { get; set; }
+        private DateTime _endDate;
+        public DateTime EndDate 
+        { 
+            get { return _endDate; }
+            set { _endDate = StartDate.AddMinutes(Duration); } 
+        }
+        public EventStatusEnum Status { get; set; } = EventStatusEnum.ABERTO;
+        private int _cost;
+        public int Cost {
+            get { return _cost; }
+            set { _cost = (int) (Duration * 0.5); }
+        }
     }
 }
