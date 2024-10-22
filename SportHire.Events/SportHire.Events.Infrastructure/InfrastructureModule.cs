2bb622e864b0cc95d3e3565f05a22d4afb6c1cb1
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-22 15:20:49 -0300

    Implemented Services injection

--- SportHire.Events/SportHire.Events.Infrastructure/InfrastructureModule.cs
+++ SportHire.Events/SportHire.Events.Infrastructure/InfrastructureModule.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using SportHire.Events.Core.Repositories;
+using SportHire.Events.Infrastructure.Persistence;
+using SportHire.Events.Infrastructure.Persistence.Repositories;
 
 namespace SportHire.Events.Infrastructure
 {
     public static class InfrastructureModule
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) 
+        {
+        }
+
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            services.AddSingleton<MongoDbOptions>(sp =>
+            {
+                var configuration = sp.GetService<IConfiguration>();
+                var options = new MongoDbOptions();
+
+                configuration.GetSection("Mongo").Bind(options);
+
+                return options;
+            });
+
+            services.AddSingleton<IMongoClient>(sp =>
+            {
+                var configuration = sp.GetService<IConfiguration>();
+                var options = sp.GetService<MongoDbOptions>();
+
+                var client = new MongoClient(options.ConnectionString);
+
+                return client;
+            });
+
+            services.AddTransient(sp =>
+            {
+                var options = sp.GetService<MongoDbOptions>();
+                var mongoClient = sp.GetService<IMongoClient>();
+
+                var db = mongoClient.GetDatabase(options.Database);
+
+                return db;
+            });
+
+            return services;
+        }
+
+        private static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IEventRepository, EventRepository>();
+
+            return services;
+        }
     }
 }
