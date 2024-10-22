5905c1b4a0eb55df64aa6bfc50a57f8f2f691ef0
eduardocaas <eduardofc.dev@gmail.com>
2024-10-21 23:12:52 -0300

    Implemented interface IEventRepository

--- SportHire.Events/SportHire.Events.Infrastructure/Persistence/Repositories/EventRepository.cs
+++ SportHire.Events/SportHire.Events.Infrastructure/Persistence/Repositories/EventRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace SportHire.Events.Infrastructure.Persistence.Repositories
+﻿using SportHire.Events.Core.Entities;
+using SportHire.Events.Core.Repositories;
+
+namespace SportHire.Events.Infrastructure.Persistence.Repositories
 {
-    public class EventRepository
+    public class EventRepository : IEventRepository
     {
+        public Task<List<Event>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
