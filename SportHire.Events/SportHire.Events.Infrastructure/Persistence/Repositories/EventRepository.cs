5b63d3a0cec2ea1d576532030f007949f9b0d33d
eduardocaas <eduardofc.dev@gmail.com>
2024-10-22 12:08:07 -0300

    Add database and collection

--- SportHire.Events/SportHire.Events.Infrastructure/Persistence/Repositories/EventRepository.cs
+++ SportHire.Events/SportHire.Events.Infrastructure/Persistence/Repositories/EventRepository.cs
@@ -1,10 +1,17 @@
-﻿using SportHire.Events.Core.Entities;
+﻿using MongoDB.Driver;
+using SportHire.Events.Core.Entities;
 using SportHire.Events.Core.Repositories;
 
 namespace SportHire.Events.Infrastructure.Persistence.Repositories
 {
     public class EventRepository : IEventRepository
     {
+        private readonly IMongoCollection<Event> _collection;
+        public EventRepository(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<Event>("events");
+        }
+
         public Task<List<Event>> GetAllAsync()
         {
             throw new NotImplementedException();
