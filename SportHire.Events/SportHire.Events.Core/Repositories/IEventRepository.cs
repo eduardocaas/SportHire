3bad8ee95ce478dc908d3529a81c877687d9157c
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-22 16:44:11 -0300

    Add method GetAllByCityAndSportAsync

--- SportHire.Events/SportHire.Events.Core/Repositories/IEventRepository.cs
+++ SportHire.Events/SportHire.Events.Core/Repositories/IEventRepository.cs
@@ -1,9 +1,11 @@
 ﻿using SportHire.Events.Core.Entities;
+using SportHire.Events.Core.Enums;
 
 namespace SportHire.Events.Core.Repositories
 {
     public interface IEventRepository
     {
+        Task<List<Event>> GetAllByCityAndSportAsync(string city, EventSportEnum sport);
         Task<List<Event>> GetAllByOwnerAsync(string ownerEmail);
     }
 }
--- SportHire.Events/SportHire.Events.Infrastructure/Persistence/Repositories/EventRepository.cs
+++ SportHire.Events/SportHire.Events.Infrastructure/Persistence/Repositories/EventRepository.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using SportHire.Events.Core.Entities;
+using SportHire.Events.Core.Enums;
 using SportHire.Events.Core.Repositories;
 
 namespace SportHire.Events.Infrastructure.Persistence.Repositories
@@ -12,9 +13,14 @@ namespace SportHire.Events.Infrastructure.Persistence.Repositories
             _collection = database.GetCollection<Event>("events");
         }
 
+        public async Task<List<Event>> GetAllByCityAndSportAsync(string city, EventSportEnum sport)
+        {
+            return await _collection.Find(e => e.City.Equals(city, StringComparison.OrdinalIgnoreCase) && e.Sport.Equals(sport)).ToListAsync();
+        }
+
         public async Task<List<Event>> GetAllByOwnerAsync(string ownerEmail)
         {
-            return await _collection.Find(c => c.EmailOwner.Equals(ownerEmail, StringComparison.OrdinalIgnoreCase)).ToListAsync();
+            return await _collection.Find(e => e.EmailOwner.Equals(ownerEmail, StringComparison.OrdinalIgnoreCase)).ToListAsync();
         }
     }
 }
