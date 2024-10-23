05ae9ae5217b0fa3e4a8090f6dd70b53dbaf5d5e
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-23 16:01:28 -0300

    Refactor options

--- SportHire.Events/SportHire.Events.Core/Enums/EventSportEnum.cs
+++ SportHire.Events/SportHire.Events.Core/Enums/EventSportEnum.cs
@@ -2,10 +2,8 @@
 {
     public enum EventSportEnum
     {
-        FUTEBOL = 0,
-        VOLEI = 1,
+        FUTEBOL = 1,
         VOLEI_PRAIA = 2,
-        FUTEVOLEI = 3,
-        BASQUETE = 4
+        BASQUETE = 3
     }
 }
