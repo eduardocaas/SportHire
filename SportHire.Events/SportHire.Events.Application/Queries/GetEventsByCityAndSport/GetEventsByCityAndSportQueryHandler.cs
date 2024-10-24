3f802b0e23f8948bea9dc1f00dbb173728fff439
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-24 15:30:20 -0300

    Implemented Handle method

--- SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCityAndSport/GetEventsByCityAndSportQueryHandler.cs
+++ SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCityAndSport/GetEventsByCityAndSportQueryHandler.cs
@@ -1,13 +1,27 @@
 ﻿using MediatR;
 using SportHire.Events.Application.ViewModels.Events;
+using SportHire.Events.Core.Repositories;
 
 namespace SportHire.Events.Application.Queries.GetEventsByCityAndSport
 {
     public class GetEventsByCityAndSportQueryHandler : IRequestHandler<GetEventsByCityAndSportQuery, List<EventPlayerViewModel>>
     {
-        public Task<List<EventPlayerViewModel>> Handle(GetEventsByCityAndSportQuery request, CancellationToken cancellationToken)
+        private readonly IEventRepository _repository;
+
+        public GetEventsByCityAndSportQueryHandler(IEventRepository repository)
         {
-            throw new NotImplementedException();
+            _repository = repository;
+        }
+
+        public async Task<List<EventPlayerViewModel>> Handle(GetEventsByCityAndSportQuery request, CancellationToken cancellationToken)
+        {
+            var events = await _repository.GetAllByCityAndSportAsync(request.City, request.Sport);
+
+            var eventsViewModel = events
+                .Select(e => new EventPlayerViewModel(e.Id, e.Sport, e.City, e.Address, e.StartDate, e.Duration, e.Cost))
+                .ToList();
+
+            return eventsViewModel;
         }
     }
 }
