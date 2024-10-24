b965e88eadeeb02db57ec4bd76479d5d2a225c0d
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-24 13:54:14 -0300

    Implemented interface

--- SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCityAndSport/GetEventsByCityAndSportQueryHandler.cs
+++ SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCityAndSport/GetEventsByCityAndSportQueryHandler.cs
@@ -1,6 +1,13 @@
-﻿namespace SportHire.Events.Application.Queries.GetEventsByCityAndSport
+﻿using MediatR;
+using SportHire.Events.Application.ViewModels.Events;
+
+namespace SportHire.Events.Application.Queries.GetEventsByCityAndSport
 {
-    public class GetEventsByCityAndSportQueryHandler
+    public class GetEventsByCityAndSportQueryHandler : IRequestHandler<GetEventsByCityAndSportQuery, List<EventPlayerViewModel>>
     {
+        public Task<List<EventPlayerViewModel>> Handle(GetEventsByCityAndSportQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
