c36fa26c808d7ba600f39b54bbb015aa496806f3
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-24 16:06:32 -0300

    Add EventsController.cs

---
+++ SportHire.Events/SportHire.Events.API/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportHire.Events.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class EventsController : ControllerBase
+    {
+    }
+}
