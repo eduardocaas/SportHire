f72074e448ef201d01a919a585dded4d3d13eabe
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-24 16:18:36 -0300

    Implemented endpoint GetByCityAndSport

--- SportHire.Events/SportHire.Events.API/Controllers/EventsController.cs
+++ SportHire.Events/SportHire.Events.API/Controllers/EventsController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SportHire.Events.Application.Queries.GetEventsByCityAndSport;
+using SportHire.Events.Core.Enums;
 
 namespace SportHire.Events.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("events")]
     [Authorize]
     public class EventsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public EventsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetByCityAndSport(
+            [FromQuery(Name = "city")] string city, 
+            [FromQuery(Name = "sport")] EventSportEnum sport)
+        {
+            var query = new GetEventsByCityAndSportQuery(city, sport);
+
+            var events = await _mediator.Send(query);
+
+            return Ok(events);
+        }
     }
 }
