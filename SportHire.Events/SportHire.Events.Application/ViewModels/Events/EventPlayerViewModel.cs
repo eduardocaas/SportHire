ef982169f32afc9e73d615e375cc900e991cc5c5
eduardocaas <eduardofc.dev@gmail.com>
2024-10-24 11:45:04 -0300

    Set properties

--- SportHire.Events/SportHire.Events.Application/ViewModels/Events/EventPlayerViewModel.cs
+++ SportHire.Events/SportHire.Events.Application/ViewModels/Events/EventPlayerViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace SportHire.Events.Application.ViewModels.Events
+﻿using SportHire.Events.Core.Enums;
+
+namespace SportHire.Events.Application.ViewModels.Events
 {
     public class EventPlayerViewModel
     {
+        public string Id { get; private set; }
+        public EventSportEnum Sport { get; private set; }
+        public string City { get; private set; }
+        public string Address { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public int Duration { get; private set; }
+        public int Cost { get; private set; }
     }
 }
