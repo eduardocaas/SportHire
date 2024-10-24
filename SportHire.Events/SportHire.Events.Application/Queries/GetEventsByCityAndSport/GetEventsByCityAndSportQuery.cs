1d266dc6fdb46a08983828ddcceaef8a5d05fc4d
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-24 13:38:25 -0300

    Set properties

--- SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCityAndSport/GetEventsByCityAndSportQuery.cs
+++ SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCityAndSport/GetEventsByCityAndSportQuery.cs
@@ -1,6 +1,18 @@
-﻿namespace SportHire.Events.Application.Queries.GetEventsByCityAndSport
+﻿using MediatR;
+using SportHire.Events.Application.ViewModels.Events;
+using SportHire.Events.Core.Enums;
+
+namespace SportHire.Events.Application.Queries.GetEventsByCityAndSport
 {
-    public class GetEventsByCityAndSportQuery
+    public class GetEventsByCityAndSportQuery : IRequest<List<EventPlayerViewModel>>
     {
+        public GetEventsByCityAndSportQuery(string city, EventSportEnum sport)
+        {
+            City = city;
+            Sport = sport;
+        }
+
+        public string City { get; private set; }
+        public EventSportEnum Sport { get; private set; }
     }
 }
