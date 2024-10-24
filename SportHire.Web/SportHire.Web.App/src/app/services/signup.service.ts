8b7e3fede24b8de21fcd81316471205f7ab5191a
Eduardo Castro <eduardofc.dev@gmail.com>
2024-10-23 23:46:11 -0300

    Implemented method Signup

--- SportHire.Web/SportHire.Web.App/src/app/services/signup.service.ts
+++ SportHire.Web/SportHire.Web.App/src/app/services/signup.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserSignup } from '../models/signup';
+import { IDENTITY_CONFIG } from '../configs/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signup(user: UserSignup) {
+    return this.http.post(`${IDENTITY_CONFIG.localUrl}/identity/users/signup`, user, {
+      observe: 'response',
+      responseType: 'text'
+    });
+  }
 }
