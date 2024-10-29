3a6c0a0056a65c35da9684f744dcd3bd8d489419
eduardocaas <eduardofc.dev@gmail.com>
2024-10-28 23:05:35 -0300

    Set properties

--- SportHire.Events/SportHire.Events.Application/Commands/CreateEvent/CreateEventCommand.cs
+++ SportHire.Events/SportHire.Events.Application/Commands/CreateEvent/CreateEventCommand.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using SportHire.Events.Core.Enums;
 
 namespace SportHire.Events.Application.Commands.CreateEvent
 {
-    internal class CreateEventCommand
+    public class CreateEventCommand : IRequest<int>
     {
+        public string EmailOwner{ get; set; }
+        public EventSportEnum Sport { get; set; }
+        public UF Uf { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Duration { get; set; }
+        public int Cost { get; set; }
     }
 }
