b0e2cf98f266e55613565ae7fb34061a64b49b7a
Eduardo <eduardofc.dev@gmail.com>
2024-10-31 23:07:35 -0300

    Set properties

--- SportHire.Web/SportHire.Web.App/src/app/models/event.ts
+++ SportHire.Web/SportHire.Web.App/src/app/models/event.ts
@@ -1,3 +1,13 @@
 export interface Event {
-
+  Id?: string;
+  Sport: number;
+  EmailOwner?: string;
+  EmailPlayer?: string;
+  Uf?: number;
+  City: string;
+  Address: string;
+  StartDate: Date;
+  Duration: number;
+  Status?: number;
+  Cost: number;
 }
