b5e07a7c55f7d90c212f246b5463b36485eebba0
eduardocaas <eduardofc.dev@gmail.com>
2024-11-04 11:01:32 -0300

    Implemented interface

--- SportHire.Web/SportHire.Web.App/src/app/services/event.service.ts
+++ SportHire.Web/SportHire.Web.App/src/app/services/event.service.ts
@@ -1,23 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Sport } from '../models/enums/sport';
 import { eventsFindPortoAlegreAberto } from '../mocks/events.mock';
+import { IEventService } from './interfaces/i.event.service';
+import { Event } from '../models/event';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class EventService implements IEventService {
 
   constructor() { }
+
+  getByCityAndSport(city: string, sport: Sport | null): Event[] {
+    throw new Error('Method not implemented.');
+  }
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class MockEventService {
+export class MockEventService implements IEventService {
 
   constructor() { }
 
-  getByCityAndSport(city: string, sport: Sport | null) {
+  getByCityAndSport(city: string, sport: Sport | null): Event[] {
     return eventsFindPortoAlegreAberto.filter(e => {
       return e.City == city && e.Sport == sport
     });
--- SportHire.Web/SportHire.Web.App/src/app/services/interfaces/i.event.service.ts
+++ SportHire.Web/SportHire.Web.App/src/app/services/interfaces/i.event.service.ts
@@ -1,4 +1,5 @@
 import { Sport } from "../../models/enums/sport";
+import { Event } from "../../models/event";
 
 export interface IEventService {
   getByCityAndSport(city: string, sport: Sport | null): Event[];
