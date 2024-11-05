fe186ea3fbafb30d914467ee5a0cfbf75665bfda
eduardocaas <eduardofc.dev@gmail.com>
2024-11-05 11:51:05 -0300

    Add params

--- SportHire.Web/SportHire.Web.App/src/app/services/event.service.ts
+++ SportHire.Web/SportHire.Web.App/src/app/services/event.service.ts
@@ -3,16 +3,21 @@ import { Sport } from '../models/enums/sport';
 import { eventsFindPortoAlegreAberto } from '../mocks/events.mock';
 import { IEventService } from './interfaces/i.event.service';
 import { Event } from '../models/event';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService implements IEventService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getByCityAndSport(city: string, sport: Sport | null): Event[] {
-    throw new Error('Method not implemented.');
+  getByCityAndSport(city: string, sport: number | null |): Event[] {
+    const params = new HttpParams().set('city', city);
+
+    if (sport != Sport.DEFAULT) {
+     params.set('sport', sport);
+    }
   }
 }
 
