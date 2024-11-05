c9064db3ccc0ccded6fb9004b22e938a67daf79e
eduardocaas <eduardofc.dev@gmail.com>
2024-11-05 11:21:48 -0300

    Implemented GetEventsByCity queries

---
+++ SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCity/GetEventsByCityQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SportHire.Events.Application.ViewModels.Events;
+
+namespace SportHire.Events.Application.Queries.GetEventsByCity
+{
+    public class GetEventsByCityQuery : IRequest<List<EventPlayerViewModel>>
+    {
+        public GetEventsByCityQuery(string city)
+        {
+            this.city = city;
+        }
+
+        public string city { get; private set; }
+    }
+}
---
+++ SportHire.Events/SportHire.Events.Application/Queries/GetEventsByCity/GetEventsByCityQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SportHire.Events.Application.ViewModels.Events;
+
+namespace SportHire.Events.Application.Queries.GetEventsByCity
+{
+    public class GetEventsByCityQueryHandler : IRequestHandler<GetEventsByCityQuery, List<EventPlayerViewModel>>
+    {
+        public Task<List<EventPlayerViewModel>> Handle(GetEventsByCityQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
