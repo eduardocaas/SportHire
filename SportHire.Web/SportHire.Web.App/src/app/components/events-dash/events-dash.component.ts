a1a04098c15a0b3701c401da3db61336d341c5ca
eduardocaas <eduardofc.dev@gmail.com>
2024-11-08 11:57:28 -0300

    Implemented Create Card design

--- SportHire.Web/SportHire.Web.App/src/app/components/events-dash/events-dash.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/events-dash/events-dash.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
 
 @Component({
   selector: 'app-events-dash',
   templateUrl: './events-dash.component.html',
-  styleUrl: './events-dash.component.css'
+  styleUrl: './events-dash.component.css',
+  providers: [{
+    provide: MAT_RADIO_DEFAULT_OPTIONS,
+    useValue: { color: 'warn' },
+  }]
 })
 export class EventsDashComponent {
 
