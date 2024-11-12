0ab80017b5a50b4bc582024bc03624fe269becbe
eduardocaas <eduardofc.dev@gmail.com>
2024-11-12 00:27:22 -0300

    Add query for Events Dash

---
+++ SportHire.Events/SportHire.Events.Application/Queries/GetEventsByOwner/GetEventsByOwnerQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SportHire.Events.Application.ViewModels.Events;
+
+namespace SportHire.Events.Application.Queries.GetEventsByOwner
+{
+    public class GetEventsByOwnerQuery : IRequest<List<EventDashViewModel>>
+    {
+        public GetEventsByOwnerQuery(string emailOwner)
+        {
+            this.emailOwner = emailOwner;
+        }
+
+        public string emailOwner { get; private set; }
+    }
+}
---
+++ SportHire.Events/SportHire.Events.Application/ViewModels/Events/EventDashViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SportHire.Events.Core.Enums;
+
+namespace SportHire.Events.Application.ViewModels.Events
+{
+    public class EventDashViewModel
+    {
+        public EventDashViewModel(
+            int id, 
+            EventSportEnum sport, 
+            string city, 
+            string district,
+            string address,
+            DateTime startDate, 
+            int duration,
+            int cost, 
+            string namePlayer)
+        {
+            Id = id;
+            Sport = sport;
+            City = city;
+            District = district;
+            Address = address;
+            StartDate = startDate;
+            Duration = duration;
+            Cost = cost;
+            NamePlayer = namePlayer;
+        }
+
+        public int Id { get; private set; }
+        public EventSportEnum Sport { get; private set; }
+        public string City { get; private set; }
+        public string District { get; private set; }
+        public string Address { get; private set; }
+        public DateTime StartDate { get; private set; }
+        public int Duration { get; private set; }
+        public int Cost { get; private set; }
+        public string NamePlayer { get; private set; }
+    }
+}
