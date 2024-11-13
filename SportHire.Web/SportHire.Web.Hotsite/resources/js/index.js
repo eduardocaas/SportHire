b3ed7b3602cd1890e4f6971f79c22bbc615d2d63
Eduardo <eduardofc.dev@gmail.com>
2024-11-12 21:17:30 -0300

    Implemented cards carousel

---
+++ SportHire.Web/SportHire.Web.Hotsite/resources/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.card-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+     autoplaySpeed: 2000
+  });
+})
