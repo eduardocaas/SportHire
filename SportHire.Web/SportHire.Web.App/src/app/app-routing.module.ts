75c8a9d2d752709af06dd3e5ce6451f167d8e41e
eduardocaas <eduardofc.dev@gmail.com>
2024-11-21 13:49:15 -0300

    Fix routing for HomeComponent

--- SportHire.Web/SportHire.Web.App/src/app/app-routing.module.ts
+++ SportHire.Web/SportHire.Web.App/src/app/app-routing.module.ts
@@ -12,17 +12,17 @@ const routes: Routes = [
 
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent, canActivate: [authGuard], children: [
-    {
-      path: 'events', component: NavComponent, children: [
-        { path: 'find', component: EventsFindComponent },
-        { path: 'dash', component: EventsDashComponent }
+  {
+    path: '', component: HomeComponent, canActivate: [authGuard],
+      children: [
+        { path: 'events', component: NavComponent, children: [
+          { path: 'find', component: EventsFindComponent },
+          { path: 'dash', component: EventsDashComponent }
+        ] }
+
       ]
-    }
-  ] },
-
+  }
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- SportHire.Web/SportHire.Web.App/src/app/components/home/home.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  showContainer = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showContainer = this.router.url === '/';
+      }
+    });
+  }
 
 }
