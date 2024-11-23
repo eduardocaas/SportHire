1f1a74ec565bdc6df5522fd858f01f9e82936e60
eduardocaas <eduardofc.dev@gmail.com>
2024-11-22 22:29:21 -0300

    Add logout function

--- SportHire.Web/SportHire.Web.App/src/app/components/nav/nav.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class NavComponent {
 
+  constructor(private _router: Router, private _authService: AuthService, private _toast: ToastrService) { }
+
+  logout() {
+    this._authService.logout();
+    this._router.navigate(['login']);
+    this._toast.info('Deslogado com sucesso' , 'Logout' , { timeOut: 3000, positionClass: 'toast-top-left' });
+  }
 }
