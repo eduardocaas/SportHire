9ab814dc048e37bab90309135dcd70076555d954
eduardocaas <eduardofc.dev@gmail.com>
2024-11-23 12:28:26 -0300

    Fix formattedTime issue

--- SportHire.Web/SportHire.Web.App/src/app/components/events/events-dash/dialog-edit/dialog-edit.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/events/events-dash/dialog-edit/dialog-edit.component.ts
@@ -18,8 +18,10 @@ export class DialogEditComponent {
   durationFormControl = new FormControl('', [Validators.required, Validators.min(30), Validators.max(180)]);
 
   get formattedTime(): string {
-    const hours = this.data.startDate.getHours();
-    const minutes = this.data.startDate.getMinutes();
+    const startDate = this.data.startDate instanceof Date ? this.data.startDate : new Date(this.data.startDate);
+
+    const hours = startDate.getHours();
+    const minutes = startDate.getMinutes();
 
     return `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
   }
