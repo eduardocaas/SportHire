bf758595f9af986b892ec56c781a9b578ff842ac
eduardocaas <eduardofc.dev@gmail.com>
2024-11-23 22:00:07 -0300

    Implemented UpdateEventCommand and Handler

---
+++ SportHire.Events/SportHire.Events.Application/Commands/UpdateEvent/UpdateEventCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SportHire.Events.Core.Enums;
+
+namespace SportHire.Events.Application.Commands.UpdateEvent
+{
+    public class UpdateEventCommand : IRequest<bool>
+    {
+        public UpdateEventCommand(string idRoute)
+        {
+            IdRoute = idRoute;
+        }
+
+        public string? IdRoute { get; set; }
+
+        public string Id { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Duration { get; set; }
+        public string? Observation { get; set; }
+    }
+}
---
+++ SportHire.Events/SportHire.Events.Application/Commands/UpdateEvent/UpdateEventCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SportHire.Events.Core.Entities;
+using SportHire.Events.Core.Repositories;
+
+namespace SportHire.Events.Application.Commands.UpdateEvent
+{
+    public class UpdateEventCommandHandler : IRequestHandler<UpdateEventCommand, bool>
+    {
+        private readonly IEventRepository _repository;
+
+        public UpdateEventCommandHandler(IEventRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Handle(UpdateEventCommand request, CancellationToken cancellationToken)
+        {
+            var _event = new Event(request.Id, request.District, request.Address, request.StartDate, request.Duration, request.Observation);
+            var result = await _repository.UpdateAsync(request.IdRoute, _event);
+            return result;
+        }
+    }
+}
