e965a991815090b87a0dff473ec6d3db18a23a51
Eduardo Castro <eduardofc.dev@gmail.com>
2024-11-26 15:44:42 -0300

    Implemented nav button dynamic content

--- SportHire.Web/SportHire.Web.App/src/app/components/nav/nav.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/nav/nav.component.ts
@@ -1,20 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DialogLogoutComponent } from './dialog-logout/dialog-logout.component';
 import { MatDialog } from '@angular/material/dialog';
 import { NoopScrollStrategy } from '@angular/cdk/overlay';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  public href: string = "";
+  public btn_text: string = "";
 
-  openLogoutDialog() {
+  constructor(
+    public dialog: MatDialog,
+    private _router: Router
+  ) { }
+
+  ngOnInit(): void {
+    // Inicializa a URL no momento da criação do componente
+    this.href = this._router.url;
+    this.updateButtonText(); // Define o texto do botão na inicialização
+
+    // Observa mudanças na rota e atualiza href e btn_text
+    this._router.events
+      .pipe(
+        filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.href = event.urlAfterRedirects;
+        this.updateButtonText();
+      });
+  }
+
+  /**
+   * Atualiza o texto do botão com base na URL atual.
+   */
+  private updateButtonText(): void {
+    if (this.href === '/events/dash') {
+      this.btn_text = 'QUERO JOGAR';
+    } else if (this.href === '/events/find') {
+      this.btn_text = 'QUERO ALUGAR';
+    } else {
+      this.btn_text = ''; // Valor padrão ou vazio
+    }
+  }
+
+  /**
+   * Redireciona entre "events/dash" e "events/find" ao clicar no botão.
+   */
+  onButtonClick(): void {
+    if (this.href === '/events/dash') {
+      this._router.navigate(['/events/find']);
+    } else if (this.href === '/events/find') {
+      this._router.navigate(['/events/dash']);
+    }
+  }
+
+  openLogoutDialog(): void {
     this.dialog.open(DialogLogoutComponent, {
-        scrollStrategy: new NoopScrollStrategy()
+      scrollStrategy: new NoopScrollStrategy()
     });
   }
 }
