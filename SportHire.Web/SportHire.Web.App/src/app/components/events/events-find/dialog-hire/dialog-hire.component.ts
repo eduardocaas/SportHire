5d0cde9d0d19c098645bec07f810f0acf384e386
Eduardo Castro <eduardofc.dev@gmail.com>
2024-11-26 14:07:32 -0300

    Implemented feature Update Player (hire)

--- SportHire.Web/SportHire.Web.App/src/app/components/events/events-find/dialog-hire/dialog-hire.component.ts
+++ SportHire.Web/SportHire.Web.App/src/app/components/events/events-find/dialog-hire/dialog-hire.component.ts
@@ -2,7 +2,10 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Sport } from '../../../../models/enums/sport';
 import { Event } from '../../../../models/event';
-import { NoopScrollStrategy } from '@angular/cdk/overlay';
+import { EventService } from '../../../../services/event.service';
+import { timer } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-hire',
@@ -13,21 +16,43 @@ export class DialogHireComponent {
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: Event,
-    ) {}
+    private readonly _service: EventService,
+    private _toast: ToastrService,
+    private _router: Router
+  ) { }
 
   isChecked = false;
 
-  // Retorna o titulo para determinado esporte - card
-  loadTitle(sport: Sport) {
-    switch (sport) {
-      case Sport.BASQUETE:
-        return 'Basquete';
-      case Sport.FUTEBOL:
-        return 'Futebol';
-      case Sport.VOLEI_PRAIA:
-        return 'Vôlei de Praia';
-      default:
-        return '';
-    }
+  updatePlayer() {
+    this._service.updatePlayer(this.data.id!).subscribe(response => {
+      this._toast.success('Inscrição confirmada com sucesso!', 'Evento', { positionClass: 'toast-bottom-center' });
+      timer(1000).subscribe(x => { this._router.navigate(['events/dash']) })
+    }, err => {
+      if (err.status == 404) {
+        this._toast.error('Evento não encontrado', 'Erro', { positionClass: 'toast-bottom-center' });
+      }
+      else if (err.error.errors) {
+        err.error.errors.forEach((e: { message: string | undefined; }) => {
+          this._toast.error(e.message, 'Erro ao inscrever', { positionClass: 'toast-bottom-center' });
+        });
+      }
+      else {
+        this._toast.error(err.error.message, 'Erro ao inscrever', { positionClass: 'toast-bottom-center' });
+      }
+    });
   }
+
+// Retorna o titulo para determinado esporte - card
+loadTitle(sport: Sport) {
+  switch (sport) {
+    case Sport.BASQUETE:
+      return 'Basquete';
+    case Sport.FUTEBOL:
+      return 'Futebol';
+    case Sport.VOLEI_PRAIA:
+      return 'Vôlei de Praia';
+    default:
+      return '';
+  }
+}
 }
