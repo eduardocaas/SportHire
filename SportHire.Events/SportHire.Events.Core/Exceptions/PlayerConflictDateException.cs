43dc087d482d1572092e9417d24baf9e3adf6e5b
eduardocaas <eduardofc.dev@gmail.com>
2024-11-27 16:22:22 -0300

    Add PlayerConflictDateException

---
+++ SportHire.Events/SportHire.Events.Core/Exceptions/PlayerConflictDateException.cs
@@ -0,0 +1,9 @@
+﻿namespace SportHire.Events.Core.Exceptions
+{
+    public class PlayerConflictDateException : Exception
+    {
+        public PlayerConflictDateException(string? message) : base(message)
+        {
+        }
+    }
+}
