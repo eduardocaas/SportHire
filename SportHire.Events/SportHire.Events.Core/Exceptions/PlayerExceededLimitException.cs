146b92c17015cce279b5184fa60b10fbfd5bacab
eduardocaas <eduardofc.dev@gmail.com>
2024-11-27 16:11:43 -0300

    Add PlayerExceededLimitException exception

---
+++ SportHire.Events/SportHire.Events.Core/Exceptions/PlayerExceededLimitException.cs
@@ -0,0 +1,9 @@
+﻿namespace SportHire.Events.Core.Exceptions
+{
+    public class PlayerExceededLimitException : Exception
+    {
+        public PlayerExceededLimitException(string? message) : base(message)
+        {
+        }
+    }
+}
