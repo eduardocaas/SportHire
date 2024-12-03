ef1d4f26ef0dfd856dec355aeb677284cac455a7
eduardocaas <eduardofc.dev@gmail.com>
2024-12-02 22:38:43 -0300

    Responsive carousel

--- SportHire.Web/SportHire.Web.Hotsite/resources/js/index.js
+++ SportHire.Web/SportHire.Web.Hotsite/resources/js/index.js
@@ -8,3 +8,14 @@ $(document).ready(function () {
      autoplaySpeed: 2000
   });
 })
+
+$(document).ready(function () {
+  $('.card-slider-one').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+     autoplaySpeed: 2000
+  });
+})
