8b9847c6724ef869a8335c3b3b90c4132e24f278
eduardocaas <eduardofc.dev@gmail.com>
2024-12-03 15:26:15 -0300

    Fix issue in DB connect and controller auth

--- SportHire.Identity/SportHire.Identity.Infrastructure/Persistence/Migrations/20241203180137_ver3.Designer.cs
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Persistence/Migrations/20241203180137_ver3.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SportHire.Identity.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using SportHire.Identity.Infrastructure.Persistence;
 namespace SportHire.Identity.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(IdentityDbContext))]
-    partial class IdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241203180137_ver3")]
+    partial class ver3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Persistence/Migrations/20241203180137_ver3.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportHire.Identity.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ver3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallet_Users_UserId",
+                table: "Wallet");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Wallet",
+                table: "Wallet");
+
+            migrationBuilder.RenameTable(
+                name: "Wallet",
+                newName: "Wallets");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallet_UserId",
+                table: "Wallets",
+                newName: "IX_Wallets_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Wallets",
+                table: "Wallets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Wallets_Users_UserId",
+                table: "Wallets");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Wallets",
+                table: "Wallets");
+
+            migrationBuilder.RenameTable(
+                name: "Wallets",
+                newName: "Wallet");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Wallets_UserId",
+                table: "Wallet",
+                newName: "IX_Wallet_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Wallet",
+                table: "Wallet",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Wallet_Users_UserId",
+                table: "Wallet",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
