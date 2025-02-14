fbeed8241f5c6b10195aa28d80e5623fb3488212
eduardocaas <eduardofc.dev@gmail.com>
2025-02-13 23:18:26 -0300

    Add UserMother with default method

---
+++ SportHire.Identity/SportHire.Identity.UnitTests/TestData/ObjectMothers/UserMother.cs
@@ -0,0 +1,27 @@
+﻿using Bogus;
+using SportHire.Identity.Core.Entities;
+
+namespace SportHire.Identity.UnitTests.TestData.ObjectMothers
+{
+    public static class UserMother
+    {
+        private static readonly Faker _faker = new();
+
+        public static User UserWithWallet
+        {
+            get
+            {
+                var user = new User(
+                    _faker.Person.FullName,
+                    _faker.Person.Email,
+                    _faker.Internet.Password(length: 8)
+                );
+
+                var wallet = new Wallet { Balance = 0, User = user };
+                user.Wallet = wallet;
+
+                return user;
+            }
+        }
+    }
+}
