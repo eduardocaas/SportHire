8f375d1a5519fdde39b5a6ba99dd080f1c342383
eduardocaas <eduardofc.dev@gmail.com>
2025-02-18 00:25:05 -0300

    Add AuthServiceTests

---
+++ SportHire.Identity/SportHire.Identity.UnitTests/Infrastructure/Security.Tests/AuthServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using SportHire.Identity.Infrastructure.Security.Services;
+
+namespace SportHire.Identity.UnitTests.Infrastructure.Security.Tests
+{
+    public class AuthServiceTests
+    {
+        private readonly IAuthService _authService;
+
+        public AuthServiceTests(IAuthService authService)
+        {
+            _authService = new AuthService(Substitute.For<IConfiguration>());
+        }
+    }
+}
