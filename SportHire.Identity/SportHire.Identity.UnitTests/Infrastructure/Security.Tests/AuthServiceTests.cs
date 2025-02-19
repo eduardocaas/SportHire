a784ddac3d85c3f67d894cd9704b15e077556e63
eduardocaas <eduardofc.dev@gmail.com>
2025-02-19 11:49:34 -0300

    Implemented GenerateSha256 tests

--- SportHire.Identity/SportHire.Identity.UnitTests/Infrastructure/Security.Tests/AuthServiceTests.cs
+++ SportHire.Identity/SportHire.Identity.UnitTests/Infrastructure/Security.Tests/AuthServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
 using NSubstitute;
 using SportHire.Identity.Infrastructure.Security.Services;
 
@@ -8,9 +9,39 @@ namespace SportHire.Identity.UnitTests.Infrastructure.Security.Tests
     {
         private readonly IAuthService _authService;
 
-        public AuthServiceTests(IAuthService authService)
+        public AuthServiceTests()
         {
             _authService = new AuthService(Substitute.For<IConfiguration>());
+        }
+
+        [Fact]
+        public void ValidPassword_GenerateSha256Hash_ReturnHash()
+        {
+            // Arrange
+            var password = "testPassword";
+            var expectedHashed = "fd5cb51bafd60f6fdbedde6e62c473da6f247db271633e15919bab78a02ee9eb";
+
+            // Act 
+            var passwordHashed = _authService.GenerateSha256Hash(password);
+
+            // Assert
+            passwordHashed.Should().NotBeNullOrEmpty();
+            passwordHashed.Should().BeEquivalentTo(expectedHashed);
+        }
+
+        [Fact]
+        public void MultipleInputs_GenerateSha256Hash_ShouldReturnDifferentHashForDifferentInputs()
+        {
+            // Arrange
+            var password1 = "testPassword1";
+            var password2 = "testPassword2";
+
+            // Act
+            var hash1 = _authService.GenerateSha256Hash(password1);
+            var hash2 = _authService.GenerateSha256Hash(password2);
+
+            // Assert
+            hash1.Should().NotBe(hash2);
         }
     }
 }
