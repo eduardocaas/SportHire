dee12f3b173aefc220f8bf6b67ced4ec220e4daf
Eduardo <eduardofc.dev@gmail.com>
2025-02-23 23:42:09 -0300

    Add Exceptions for Private key

---
+++ SportHire.Identity/SportHire.Identity.Infrastructure/Security/Exceptions/PemKeyException.cs
@@ -0,0 +1,26 @@
+﻿namespace SportHire.Identity.Infrastructure.Security.Exceptions
+{
+    public class PrivateKeyFileNotFoundException : Exception
+    {
+        public PrivateKeyFileNotFoundException()
+            : base("O arquivo de chave privada não foi encontrado.") { }
+
+        public PrivateKeyFileNotFoundException(Exception innerException)
+            : base("O arquivo de chave privada não foi encontrado.", innerException) { }
+
+        public PrivateKeyFileNotFoundException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+
+    public class InvalidPrivateKeyException : Exception
+    {
+        public InvalidPrivateKeyException()
+            : base("A chave privada fornecida é inválida.") { }
+
+        public InvalidPrivateKeyException(Exception innerException)
+            : base("A chave privada fornecida é inválida.", innerException) { }
+
+        public InvalidPrivateKeyException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
